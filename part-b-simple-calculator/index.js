6439becc66fc534fe5ba70287cdeb27b1bb7b72e
raghavendra-musubi <raghavendra@techis.io>
2020-12-14 04:50:11 +0530

    inner text update on loading from api

--- part-b-simple-calculator/index.js
+++ part-b-simple-calculator/index.js
@@ -1,1 +1,25 @@
-console.log("!!!")
+
+// const apiUrl = 'http://api.forismatic.com/api/1.0/'
+const apiUrl = 'https://type.fit/api/quotes'
+let quotesArray 
+let quoteElement = document.getElementById("quote")
+let authorElement = document.getElementById("author")
+
+fetch(apiUrl)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(quotesArray) {
+    
+    console.log(quotesArray.length)
+
+    // load a random quote and the author
+    
+
+    // set the inner text for author and quote
+    quoteElement.innerHTML = "Here goes the quote"
+    authorElement.innerHTML = "Here goes the author"
+
+
+  });
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Part A 
+
+### API resources 
+
+- [fCC Forum Post](https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373)
+- [Free APIs - GitHub repository](https://github.com/public-apis/public-apis)
+
+***
+
+# Part B

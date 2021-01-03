be5d210acc4ad1c6712dc662071960dd4e2b8e16
raghavendra-musubi <raghavendra@techis.io>
2021-01-04 00:30:08 +0530

    calculator mostly working

--- part-b-simple-calculator/index.js
+++ part-b-simple-calculator/index.js
@@ -5,9 +5,41 @@ $("button").click(function () {
 
     if (this.innerText == '=') {
 
-        $("#eval-answer").html(eval(currentExpression))
-        currentExpression = ''
-        $("#current-expression").text('')
+        try {
+
+            currentResult = eval(currentExpression)
+
+            if (currentResult == currentExpression || currentExpression =='0.' ) {
+    
+                currentExpression = ''
+                $("#eval-answer").html('0.')
+                $("#current-expression").text(currentExpression)
+    
+    
+            } else {
+    
+                $("#eval-answer").html(currentResult)
+                currentExpression = currentResult.toString()
+                $("#current-expression").text(currentResult)
+    
+            }
+
+
+        } catch (error) {
+
+            if (error instanceof SyntaxError ) {
+
+                window.alert('Invalid operation -- Can not evaluate!')
+
+            } else {
+                
+                console.log(error)
+
+            }
+            
+
+        } 
+
 
     } else {
 
